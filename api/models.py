39e381d5bd2593fb2347c2132a90bc7b551e1775
josinbahizi <rugambajospin@gmail.com>
2021-04-26 23:37:46 +0200

    React frontend

--- api/models.py
+++ api/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=255)
     completed = models.BooleanField(default=False, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=False)
 
     def __str__(self):
         return self.title
--- api/views.py
+++ api/views.py
@@ -19,7 +19,8 @@ def apiOverview(request):
 @api_view(['GET'])
 def taskList(request):
     tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
+    tasks_ordered = tasks.order_by('-id')
+    serializer = TaskSerializer(tasks_ordered, many=True)
     return Response(serializer.data)
 
 
