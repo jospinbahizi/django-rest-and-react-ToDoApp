bb1c849aa92674232081f6f9731d93ad90758b5c
josinbahizi <rugambajospin@gmail.com>
2021-04-27 21:24:41 +0200

    Heroku deployment

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
--- ToDo/wsgi.py
+++ ToDo/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhitenoise
+# from whitenoise.django import DjangoWhitenoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToDo.settings')
 
 application = get_wsgi_application()
-application = DjangoWhitenoise(application)
+# application = DjangoWhitenoise(application)
